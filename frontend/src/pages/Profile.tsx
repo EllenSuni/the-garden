9674987d8cf53559ad6b5d70bad871a698f28f81
EllenSuni <ellen.suni@outlook.com>
2024-05-30 10:07:11 +0200

    rendering areas on profile-page

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,15 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import "./profile.css";
+
 import AddArea from "../components/AddArea";
+import { IArea } from "backend/interfaces";
+import TrashCan from "../assets/icons/trash-can.svg";
 
 function Profile() {
-  const [showAreaModal, setShowAreaModal] = useState(false);
+  const [showAreaModal, setShowAreaModal] = useState(false),
+    [areas, setAreas] = useState<IArea[]>([]);
+
+  function deleteArea(area: IArea) {
+    console.log("delete", area);
+  }
+
+  useEffect(() => {
+    fetch("/api/area")
+      .then((response) => response.json())
+      .then((result) => {
+        setAreas(result);
+        console.log(result);
+      });
+  }, []);
 
   return (
-    <>
+    <section className="profile-page">
       <h1>Profil</h1>
-      <button onClick={() => setShowAreaModal(true)}>Lägg till område</button>
-      {showAreaModal && <AddArea modal={setShowAreaModal} />}
-    </>
+      <div className="my-areas">
+        <h3>Mina områden</h3>
+        <button onClick={() => setShowAreaModal(true)}>Lägg till</button>
+        {showAreaModal && <AddArea modal={setShowAreaModal} />}
+        <div>
+          <ul>
+            {areas.map((area) => (
+              <div className="my-areas-list-row">
+                <li
+                  className="my-areas-list-row-item"
+                  key={area.id}>
+                  {area.name}
+                </li>
+                <img
+                  className="my-areas-trash-can"
+                  src={TrashCan}
+                  alt="Ta bort"
+                  onClick={() => deleteArea(area)}
+                />
+              </div>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </section>
   );
 }
 
